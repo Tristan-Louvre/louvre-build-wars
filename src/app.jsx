e4b6ecf41146ba89cfc96edd66354a3d97e44d33
Tristan <byron@wigroup.co.za>
2019-01-03 18:19:10 +0200

    Initial Commit.

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div
+        data-armory-embed="specializations"
+        data-armory-ids="1,10,23,24,40,45,59"
+        data-armory-3-traits="1761,1774,1749"
+      />
+    );
+  }
+}
