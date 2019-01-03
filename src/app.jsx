63b5b260645c2433f16bb4ef1e1ff221092bdcc8
Tristan <byron@wigroup.co.za>
2019-01-03 18:26:26 +0200

    Fixing indentation to my tastes.

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div
-        data-armory-embed="specializations"
-        data-armory-ids="1,10,23,24,40,45,59"
-        data-armory-3-traits="1761,1774,1749"
-      />
-    );
-  }
+    render() {
+        return (
+            <div
+                data-armory-embed="specializations"
+                data-armory-ids="1,10,23,24,40,45,59"
+                data-armory-3-traits="1761,1774,1749"
+            />
+        );
+    }
 }
